805106034be49b7362645c992d731c1e2b420908
GADDALA ASHISH <ashishpaul1199@gmail.com>
2025-02-15 13:59:42 +0530

    Add files via upload

---
+++ 04-Talk is Cheap, Show Me the Code/resObj.js
@@ -0,0 +1,25 @@
+const resObj={
+    "id": "686427",
+    "name": "KFC",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/87a05c2f-b366-4efc-99ec-9938e0510568_686427.JPG",
+    "locality": "Wyra Road",
+    "areaName": "Khammam",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+      "Burgers",
+      "Fast Food",
+      "Rolls & Wraps"
+    ],
+    "avgRating": 4.2,
+    "parentId": "547",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "3.0K+",
+    "sla": {
+      "deliveryTime": 30,
+      "lastMileTravel": 5.8,
+      "serviceability": "SERVICEABLE",
+      "slaString": "30-35 mins",
+      "lastMileTravelString": "5.8 km",
+      "iconType": "ICON_TYPE_EMPTY"
+   }
+  };
