1d775a4060ca0f4c2f5641161368f5b18ee1b267
GADDALA ASHISH <ashishpaul1199@gmail.com>
2025-02-18 17:43:04 +0530

    Add files via upload

---
+++ 05-Let's Get Hooked/src/utlis/constant.js
@@ -0,0 +1,5 @@
+export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL="https://raw.githubusercontent.com/ashishpaul99/Namaste-React-JS/main/04-Talk%20is%20Cheap%2C%20Show%20Me%20the%20Code/pixelcut-export%20(1)%20(1).png";
+
+
---
+++ 05-Let's Get Hooked/src/utlis/mockData.js
@@ -0,0 +1,71 @@
+const reslist = [
+  { 
+    id: "67890",
+    name: "KFC",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/87a05c2f-b366-4efc-99ec-9938e0510568_686427.JPG",
+    cuisines: ["Fried Chicken", "Fast Food"],
+    avgRating: "3.9",
+    deliveryTime: 25 ,
+  },
+  {
+    id: "12345",
+    name: "Ibaco",
+    cloudinaryImageId:"kyllhajjzwkzagnskpx6",
+    cuisines: ["Pizza", "Italian"],
+    avgRating: "4.7",
+    deliveryTime: 30,
+  },
+  {
+    id: "631211",
+    name: "Domino's Pizza",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/7/ccf02c59-edba-46b0-aff9-f3deacba54dd_631211.JPG",
+    cuisines: [
+      "Pizzas",
+      "Italian",
+      "Pastas",
+      "Desserts"
+    ],
+    avgRating: "4.2",
+    deliveryTime: 23,
+  },
+  {
+    id: "508890",
+    name: "IC Ice Creams",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/10/b6dfd4f6-80ac-4180-890b-d4ad70366d27_508890.JPG",
+    cuisines: [
+      "Ice Cream",
+      "Desserts"
+    ],
+    avgRating: "4.6",
+    deliveryTime: 26 
+  },
+  {
+    id: "266392",
+    name: "The Thick Shake Factory",
+    cloudinaryImageId: "vuv0vvek1jl99bvjveq0",
+    cuisines: [
+      "Beverages",
+      "American"
+    ],
+    avgRating: "3.10",
+    deliveryTime: 27
+  }
+];
+
+export default reslist;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
